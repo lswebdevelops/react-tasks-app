aba06c2ecbcf7b68e1a11d57b96e63b649f53cc6
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-05-16 21:10:37 -0300

    list of tasks is ready, adding styles

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
 import React, { Component } from "react";
+import uniqid from "uniqid";
+import Overview from "./Components/Overview";
+import './Styles/Components.css'
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      task: {text: ""},
+      task: {
+        text: "",
+        id: uniqid(),
+      },
       tasks: [],
     };
   }
 
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+        id: this.state.task.id,
+      },
+    });
+  };
 
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: { 
+        text: "" },
+        id: uniqid(),
+    });
+  };
   render() {
-    const {task, tasks }  = this.state;
+    const { task, tasks } = this.state;
 
-    return(
-       <div>
-        <form>
+    return (
+      <div>
+        <form onSubmit={this.onSubmitTask}>
           <label htmlFor="taskInput">Enter task</label>
-          <input type="text" id="taskInput"/>
+          <input
+            type="text"
+            id="taskInput"
+            value={task.text}
+            onChange={this.handleChange}
+          />
+
           <button>Add Task</button>
         </form>
-       </div>
-       
-       
-       
-       )
+        <Overview tasks={tasks} />
+      </div>
+    );
   }
 }
 export default App;

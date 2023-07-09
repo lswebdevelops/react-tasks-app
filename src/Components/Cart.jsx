a0ff19b005f4a04f76f8791e5562ff91b9b2fbdd
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-07-08 23:58:57 -0300

    total price added to cart page

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,35 +1,47 @@
 import React from "react";
 
 const Cart = (props) => {
-    return (
-      <div>
-        <h1>Cart</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Ticker</th>
-              <th>Name</th>
-              <th>Price 2023</th>
-              <th>Size</th>
-            </tr>
-          </thead>
-          <tbody>
-            {props.cartItems.map((item, index) => {
-              return (
-                <tr key={index}>
-                  <td>{item.id}</td>
-                  <td>{item.ticker}</td>
-                  <td>{item.name}</td>
-                  <td>{item.price2023}</td>
-                  <td>{item.size}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+  const { cartItems } = props;
+
+  // Calculate the total price
+  const totalPrice = cartItems.reduce((total, item) => {
+    return total + item.price2023;
+  }, 0);
+
+  return (
+    <div>
+      <h1>Cart</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Ticker</th>
+            <th>Name</th>
+            <th>Price 2023</th>
+            <th>Size</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cartItems.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{item.id}</td>
+                <td>{item.ticker}</td>
+                <td>{item.name}</td>
+                <td>{item.price2023}</td>
+                <td>{item.size}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      <div className="total-price">
+        <span>TOTAL: </span>
+        <span>${totalPrice.toFixed(2)}</span>
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
+
 export default Cart;
